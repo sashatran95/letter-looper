2b6b17fa826a3a1334f07d30024b089c35d29a38
Sashtastic <ht.sasha.tran@gmail.om>
2016-02-26 17:42:12 -0500

    Committing Issues

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var aLetter = prompt('Enter a letter, darling:');
+
+for (a = 0; a < 10; a++) {
+	document.write (aLetter)
+	document.write ('<br>')
+}
+if (aLetter == 1 || aLetter == 2 || aLetter == 3 || aLetter == 4 || aLetter == 5 || aLetter == 6 || aLetter == 7 || aLetter == 8 || aLetter == 9 || aLetter == 0) {
+	alert('THAT AINT A LETTER');
+}
